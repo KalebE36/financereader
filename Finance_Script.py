672fdd45e343bdf5629dd839d5ec31bdb4133ce1
Kaleb Ellington <Kaleb.ellington33@gmail.com>
2024-08-08 09:40:22 -0400

    update

--- Finance_Script.py
+++ Finance_Script.py
@@ -2,7 +2,8 @@ import pandas as pd
 import re
 
 # Load the CSV file
-file_path = 'Checking1.csv'
+file_path = '$$$$.csv'
+output_file_path = 'output.csv'
 df = pd.read_csv(file_path, header=None)
 
 relevant_columns = df[[0, 1, 4]]
@@ -10,6 +11,10 @@ relevant_columns.columns = ['Date', 'Amount', 'Description']
 
 # Function to normalize descriptions (remove dates, transaction IDs, etc.)
 def normalize_description(desc):
+    if 'Amzn.com/bill' in desc:
+        return "Amazon"
+    
+    desc = re.sub(r'ON \d{2}/\d{2} REF #\w+', '', desc)
     desc = re.sub(r'\d{2}/\d{2}', '', desc)
     desc = re.sub(r'\d+', '', desc)
     desc = re.sub(r'PURCHASE AUTHORIZED ON', '', desc, flags=re.IGNORECASE)
@@ -22,5 +27,8 @@ recurring_payments = relevant_columns.groupby('Normalized_Description').agg(
 ).reset_index()
 recurring_payments = recurring_payments[recurring_payments['Counts'] > 1]
 recurring_payments = recurring_payments.sort_values(by='Total_Amount')
+recurring_payments['Total_Amount'] = recurring_payments['Total_Amount'].map('{:.2f}'.format)
+recurring_payments.to_csv(output_file_path, index=False)
 
 print(recurring_payments)
+
